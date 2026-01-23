360b55ef26ad024aefb06805a6b2feb5d9138fae
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-23 09:11:30 +0100

    fix missing paren

--- src/utils.h
+++ src/utils.h
@@ -4,4 +4,4 @@
 
 #define PARSE_U16(lsb, msb) ((uint16_t)(lsb) | ((uint16_t)(msb) << 8))
 #define MSB_U16(x) ((uint8_t)((x) >> 8))
-#define LSB_U16(x) ((uint8_t)((x) & 0xff)
+#define LSB_U16(x) ((uint8_t)((x) & 0xff))
