8f088e0571e0df91785094f859017b492abe1823
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-23 05:03:52 +0100

    comment

--- src/main.c
+++ src/main.c
@@ -35,7 +35,8 @@ static uint8_t bus_read_port(const struct SPC_State* state, uint16_t addr)
             return state->dsp.registers[state->dsp.addr_latch & 0x7f];
 
         // TODO: latch the last value
-        // somewhere in the init RAM probably
+        // somewhere in the init RAM probably but in ram[0xf4] is the data last
+        // _written_ by the SPC, which is different from the one it will read
         case 0xf4:     // TODO: CPUI0
         case 0xf5:     // TODO: CPUI1
         case 0xf6:     // TODO: CPUI2
