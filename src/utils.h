06dafa5d4656cac9ec13add0361f8799f6409731
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-23 09:20:35 +0100

    implement more bit operations

--- src/utils.h
+++ src/utils.h
@@ -5,3 +5,15 @@
 #define PARSE_U16(lsb, msb) ((uint16_t)(lsb) | ((uint16_t)(msb) << 8))
 #define MSB_U16(x) ((uint8_t)((x) >> 8))
 #define LSB_U16(x) ((uint8_t)((x) & 0xff))
+
+#define BIT(i) (1u << (i))
+
+#define BIT_AT(x, i) (((x) >> (i)) & 1u)
+
+#define BIT_SET(x, i) ((x) |= (uint8_t)BIT(i))
+#define BIT_CLEAR(x, i) ((x) &= (uint8_t)~BIT(i))
+#define BIT_TOGGLE(x, i) ((x) ^= (uint8_t)BIT(i))
+
+// b is thought of as a bool but could be anything integral
+#define BIT_WRITE(x, i, b) \
+    ((x) = ((x) & (uint8_t)~BIT(i)) | ((uint8_t)(!!(b)) << (i)))
