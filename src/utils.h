41d6c6b80c1cc73a3556726a30c0245841117b6d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-26 00:55:31 +0100

    implement flag based bit operations

--- src/utils.h
+++ src/utils.h
@@ -6,6 +6,15 @@
 #define MSB_U16(x) ((uint8_t)((x) >> 8))
 #define LSB_U16(x) ((uint8_t)((x) & 0xff))
 
+#define FLAG_AT(x, m) (!!((x) & (m)))
+
+#define FLAG_SET(x, m) ((x) |= (uint8_t)(m))
+#define FLAG_CLEAR(x, m) ((x) &= (uint8_t) ~(m))
+#define FLAG_TOGGLE(x, m) ((x) ^= (uint8_t)(m))
+
+#define FLAG_WRITE(x, m, b) \
+    ((x) = ((x) & (uint8_t) ~(m)) | ((b) ? (uint8_t)(m) : 0))
+
 #define BIT(i) (1u << (i))
 
 #define BIT_AT(x, i) (((x) >> (i)) & 1u)
