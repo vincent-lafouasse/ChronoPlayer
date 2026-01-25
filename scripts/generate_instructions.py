9a3dcb8ff0dc59aa7e8a55ee8dfaa42c00733ddf
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-25 05:12:51 +0100

    remove dead code

--- scripts/generate_instructions.py
+++ scripts/generate_instructions.py
@@ -3,15 +3,6 @@ from dataclasses import dataclass
 from enum import Enum
 
 
-@dataclass
-class OpcodeMetadata:
-    mnemonic: str  # LDA STY etc
-    cycles: int
-    length: int  # number of bytes
-    addressing_mode: str  # bus access pattern
-    operation: str  # payload after bus access, e.g. bus write or register write
-
-
 class Register(Enum):
     A = 1
     X = 2
