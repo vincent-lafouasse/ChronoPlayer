bbf53a48815243342d1520f00d3caf0618d35b4d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-26 04:19:21 +0100

    refacor bit operations

--- src/utils.h
+++ src/utils.h
@@ -2,27 +2,26 @@
 
 #include <stdint.h>
 
-#define PARSE_U16(lsb, msb) ((uint16_t)(lsb) | ((uint16_t)(msb) << 8))
-#define MSB_U16(x) ((uint8_t)((x) >> 8))
-#define LSB_U16(x) ((uint8_t)((x) & 0xff))
+#define AS_U8(val) ((uint8_t)(val))
+#define AS_U16(val) ((uint16_t)(val))
+
+#define PARSE_U16(lsb, msb) (AS_U16(lsb) | (AS_U16(msb) << 8))
+#define MSB_U16(x) AS_U8((x) >> 8)
+#define LSB_U16(x) AS_U8(x)
 
 #define FLAG_AT(x, m) (!!((x) & (m)))
 
-#define FLAG_SET(x, m) ((x) |= (uint8_t)(m))
-#define FLAG_CLEAR(x, m) ((x) &= (uint8_t)~(m))
-#define FLAG_TOGGLE(x, m) ((x) ^= (uint8_t)(m))
+#define FLAG_SET(x, m) ((x) |= AS_U8(m))
+#define FLAG_CLEAR(x, m) ((x) &= ~AS_U8(m))
+#define FLAG_TOGGLE(x, m) ((x) ^= AS_U8(m))
 
 #define FLAG_WRITE(x, m, b) \
-    ((x) = ((x) & (uint8_t)~(m)) | ((b) ? (uint8_t)(m) : 0))
+    ((x) = AS_U8(((x) & ~AS_U8(m)) | ((b) ? AS_U8(m) : 0)))
 
 #define BIT(i) (1u << (i))
 
-#define BIT_AT(x, i) (((x) >> (i)) & 1u)
-
-#define BIT_SET(x, i) ((x) |= (uint8_t)BIT(i))
-#define BIT_CLEAR(x, i) ((x) &= (uint8_t)~BIT(i))
-#define BIT_TOGGLE(x, i) ((x) ^= (uint8_t)BIT(i))
-
-// b is thought of as a bool but could be anything integral
-#define BIT_WRITE(x, i, b) \
-    ((x) = ((x) & (uint8_t)~BIT(i)) | ((uint8_t)(!!(b)) << (i)))
+#define BIT_AT(x, i) FLAG_AT(x, BIT(i))
+#define BIT_SET(x, i) FLAG_SET(x, BIT(i))
+#define BIT_CLEAR(x, i) FLAG_CLEAR(x, BIT(i))
+#define BIT_TOGGLE(x, i) FLAG_TOGGLE(x, BIT(i))
+#define BIT_WRITE(x, i, b) FLAG_WRITE(x, BIT(i), b)
