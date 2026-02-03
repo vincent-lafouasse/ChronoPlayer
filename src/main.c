a0108ecb16b6e09271977d38c18c9c1c618835b2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-03 19:45:43 +0100

    format

--- src/main.c
+++ src/main.c
@@ -43,16 +43,16 @@ void cpu_tick(struct SPC_State* state)
 //     * Verified by blargg.
 //     * 2 and 3 could be swapped, but that would be odd.
 // */
-// 
+//
 // // returns true when done
 // // AND A, !a
 // bool and_accumulator_absolute(struct SPC_State* state, uint32_t cycle)
 // {
 //     static uint16_t addr;
-// 
+//
 //     struct CPU_State* cpu = &state->cpu;
 //     uint8_t data;
-// 
+//
 //     // cycle 1 already burned
 //     switch (cycle) {
 //         case 2:
@@ -72,7 +72,7 @@ void cpu_tick(struct SPC_State* state)
 //             return true;
 //     }
 // }
-// 
+//
 // /*
 //  22 Absolute (RMW) -- !a
 //   (ASL,DEC,INC,LSR,ROL,ROR,TCLR1,TSET1)
@@ -94,9 +94,9 @@ void cpu_tick(struct SPC_State* state)
 // {
 //     static uint16_t addr;
 //     static uint8_t data;
-// 
+//
 //     struct CPU_State* cpu = &state->cpu;
-// 
+//
 //     switch (cycle) {
 //         case 2:
 //             addr = bus_read(state, cpu->pc++);
@@ -104,7 +104,7 @@ void cpu_tick(struct SPC_State* state)
 //         case 3:
 //             addr |= (uint16_t)bus_read(state, cpu->pc++) << 8;
 //             return false;
-// 
+//
 //         case 4:
 //             data = bus_read(state, addr);
 //             return false;
@@ -113,14 +113,14 @@ void cpu_tick(struct SPC_State* state)
 //             // TODO: flags
 //             bus_write(state, addr, data);
 //             return true;
-// 
+//
 //         default:
 //             // unreachable
 //             return true;
 //     }
 // }
 // /*
-// 
+//
 //  10 Direct, Immediate -- d,#i
 //   (ADC,AND,CMP,EOR,MOV,OR,SBC)
 //   (3 bytes)
@@ -134,48 +134,46 @@ void cpu_tick(struct SPC_State* state)
 //     * Yes, RMW even for MOV.
 //     * CMP does not write for cycle 5; does it IO or read again?
 // */
-// 
+//
 // // MOV d, #i
 // bool mov_direct_immediate(struct SPC_State* state, uint32_t cycle)
 // {
 //     static uint16_t addr;
 //     static uint8_t value;
-// 
+//
 //     struct CPU_State* cpu = &state->cpu;
-// 
+//
 //     switch (cycle) {
 //         case 2:
 //             value = bus_read(state, cpu->pc++);
 //             return false;
-// 
+//
 //         case 3: {
 //             uint8_t offset = bus_read(state, cpu->pc++);
 //             uint8_t page = !!(cpu->status & PSW_P);
 //             addr = u16_parse(offset, page);  // lsb msb
 //             return false;
 //         }
-// 
+//
 //         case 4:
 //             // yes RMW even though we don't need the value we're about to
 //             // overwrite
 //             (void)bus_read(state, addr);
 //             return false;
-// 
+//
 //         case 5:
 //             bus_write(state, addr, value);
 //             return true;
-// 
+//
 //         default:
 //             return true;
 //     }
 // }
-// 
+//
 // /*
 //  24 MemBit, Carry -- m.b,C
-//   MOV1  m.b, C       CA    3     6   (m.b) = C                        ........
-//   (MOV1)
-//   (3 bytes)
-//   (6 cycles)
+//   MOV1  m.b, C       CA    3     6   (m.b) = C ........ (MOV1) (3 bytes) (6
+//   cycles)
 //         1       PC      Op Code         1
 //         2       PC+1    AAL             1
 //         3       PC+2    AAH & BIT       1
@@ -184,21 +182,21 @@ void cpu_tick(struct SPC_State* state)
 //         6       AA      Data (write)    0
 //     * Verified by blargg. 2 and 3 could be swapped, but that's unlikely.
 // */
-// 
+//
 // // MOV1 m.b, C
 // bool mov1_membit_carry(struct SPC_State* state, uint32_t cycle)
 // {
 //     static uint16_t addr;
 //     static uint8_t bit_index;
 //     static uint8_t data;
-// 
+//
 //     struct CPU_State* cpu = &state->cpu;
-// 
+//
 //     switch (cycle) {
 //         case 2:
 //             addr = bus_read(state, cpu->pc++);
 //             return false;
-// 
+//
 //         case 3: {
 //             // BBBA'AAAA
 //             // BBB indexes the bit 0-7
@@ -208,25 +206,26 @@ void cpu_tick(struct SPC_State* state)
 //             addr = u16_parse(addr, raw_hi & 0x1f);
 //             return false;
 //         }
-// 
+//
 //         case 4:
 //             // RMW Read
 //             data = bus_read(state, addr);
 //             return false;
-// 
+//
 //         case 5:
 //             // Internal Operation
 //             // RMW Modify
-//             // could be done right before the write, doesn't really matter but
+//             // could be done right before the write, doesn't really matter
+//             but
 //             // neater like this
 //             bit_write(&data, bit_index, (cpu->status & PSW_C));
 //             return false;
-// 
+//
 //         case 6:
 //             // RMW Write
 //             bus_write(state, addr, data);
 //             return true;
-// 
+//
 //         default:
 //             return true;
 //     }
