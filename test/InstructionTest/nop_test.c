3ba69fe5f5afaa2dc390f566f123e24b30edac93
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-05 00:39:42 +0100

    implement simple Bus event array

--- test/InstructionTest/nop_test.c
+++ test/InstructionTest/nop_test.c
@@ -9,6 +9,36 @@
 #define I_PENDING INSTRUCTION_STATUS_PENDING
 #define I_ERROR INSTRUCTION_STATUS_UNEXPECTED_CYCLE
 
+enum IoType {
+    IO_READ,
+    IO_WRITE,
+};
+
+struct BusEvent {
+    uint64_t total_cycles;
+    uint8_t instruction_cycle;
+    uint8_t value;
+    uint16_t addr;
+    enum IoType type;
+};
+
+#define LOG_SZ 128
+
+struct BusEventLog {
+    struct BusEvent events[LOG_SZ];
+    uint32_t len;
+    uint32_t cap;
+};
+
+void event_push(struct BusEventLog log[static 1], struct BusEvent event)
+{
+    if (log->len >= log->cap) {
+        return;
+    }
+
+    log->events[log->len++] = event;
+}
+
 UTEST(InstructionTest, H00_NOP)
 {
     struct SPC_State state;
