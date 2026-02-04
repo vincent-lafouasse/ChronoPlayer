558f95ca5d1ed75876dc358eaa779157c0b31b13
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-04 23:00:32 +0100

    actually test NOP

--- test/InstructionTest/nop_test.c
+++ test/InstructionTest/nop_test.c
@@ -5,12 +5,32 @@
 #include "instructions.gen.h"
 #include "state.h"
 
-UTEST(InstructionTest, nop_linkage)
+#define I_OK INSTRUCTION_STATUS_DONE
+#define I_PENDING INSTRUCTION_STATUS_PENDING
+#define I_ERROR INSTRUCTION_STATUS_UNEXPECTED_CYCLE
+
+UTEST(InstructionTest, H00_NOP)
 {
     struct SPC_State state;
-    memset(&state, 0, sizeof(state));
 
-    nop(&state, 2);
+    int cycle;
+    enum InstructionStatus expected;
+    enum InstructionStatus actual;
+
+    cycle = 1;
+    expected = I_ERROR;
+    actual = nop(&state, cycle);
+    ASSERT_EQ(expected, actual);
+
+    cycle = 2;
+    expected = I_OK;
+    actual = nop(&state, cycle);
+    ASSERT_EQ(expected, actual);
+
+    cycle = 3;
+    expected = I_ERROR;
+    actual = nop(&state, cycle);
+    ASSERT_EQ(expected, actual);
 }
 
 UTEST_MAIN()
