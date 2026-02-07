93e00ce1d1c7ca0b4abbfff5b456f962ee044bd3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-07 07:27:33 +0100

    add reference for filter bank

--- src/main.c
+++ src/main.c
@@ -189,9 +189,23 @@ int16_t brr_decode_sample(const struct BRR_Block block[static 1],
 
     // up to second order recursive filter
     // uses fixed point arithmetic
+    //
+    // reference:
+    // There are 4 possible 'filters' to use in decoding the blocks. Some
+    // filters use
+    // previous samples in decoding, this does carry over between groups and
+    // blocks and is separate for each voice.
+    //   Filter 0 (Direct):       S(x) = RD
+    //   Filter 1 (15/16):        S(x) = RD + S(x-1) + ((-S(x-1))>>4)
+    //   Filter 2 (61/32-15/16):  S(x) = RD + (S(x-1)<<1) +
+    //                                   ((-((S(x-1)<<1)+S(x-1)))>>5)
+    //                                      - S(x-2) + (S(x-2)>>4)
+    //   Filter 3 (115/64-13/16): S(x) = RD + (S(x-1)<<1) +
+    //                        ((-(S(x-1)+(S(x-1)<<2)+(S(x-1)<<3)))>>6)
+    //                          - S(x-2) + (((S(x-2)<<1) + S(x-2))>>4)
     switch (filter) {
         case 0:
-            // filter 0 does nothing
+            // direct: filter 0 does nothing
             break;
 
         case 1:  // 15/16 ie +1 - 1/16
