02957c1302ea6cf26025fd75ace1beabd5480cfc
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-07 04:49:44 +0100

    transcribe DSP register indices

--- src/main.c
+++ src/main.c
@@ -7,16 +7,35 @@
 
 #include "utils.h"
 
-#define DSP_V_VOLL(voice)   ((voice) << 4 | 0x0)
-#define DSP_V_VOLR(voice)   ((voice) << 4 | 0x1)
+#define DSP_V_VOLL(voice) ((voice) << 4 | 0x0)
+#define DSP_V_VOLR(voice) ((voice) << 4 | 0x1)
 #define DSP_V_PITCHL(voice) ((voice) << 4 | 0x2)
 #define DSP_V_PITCHH(voice) ((voice) << 4 | 0x3)
-#define DSP_V_SRCN(voice)   ((voice) << 4 | 0x4)
-#define DSP_V_ADSR1(voice)  ((voice) << 4 | 0x5)
-#define DSP_V_ADSR2(voice)  ((voice) << 4 | 0x6)
-#define DSP_V_GAIN(voice)   ((voice) << 4 | 0x7)
-#define DSP_V_ENVX(voice)   ((voice) << 4 | 0x8)
-#define DSP_V_OUTX(voice)   ((voice) << 4 | 0x9)
+#define DSP_V_SRCN(voice) ((voice) << 4 | 0x4)
+#define DSP_V_ADSR1(voice) ((voice) << 4 | 0x5)
+#define DSP_V_ADSR2(voice) ((voice) << 4 | 0x6)
+#define DSP_V_GAIN(voice) ((voice) << 4 | 0x7)
+#define DSP_V_ENVX(voice) ((voice) << 4 | 0x8)
+#define DSP_V_OUTX(voice) ((voice) << 4 | 0x9)
+
+#define DSP_MVOLL (0x0c)
+#define DSP_MVOLR (0x1c)
+#define DSP_EVOLL (0x2c)
+#define DSP_EVOLR (0x3c)
+#define DSP_KON (0x4c)
+#define DSP_KOFF (0x5c)
+#define DSP_FLG (0x6c)
+#define DSP_ENDX (0x7c)
+
+#define DSP_EFB (0x0d)
+#define DSP_PMON (0x2d)
+#define DSP_NON (0x3d)
+#define DSP_EON (0x4d)
+#define DSP_DIR (0x5d)
+#define DSP_ESA (0x6d)
+#define DSP_EDL (0x7d)
+
+#define DSP_EFFC(i) ((i) << 4 | 0xf)
 
 struct BRR_Sample {
     uint16_t start;  // where the sample starts
