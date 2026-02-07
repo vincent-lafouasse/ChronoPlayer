8fa0f7509041af922bc8878eb31511e1b853d8e6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-07 03:18:02 +0100

    format python as well

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -o xtrace
-
 TO_FORMAT=$(find src test -name '*.c' -or -name '*.h' | grep -v '\.gen\.' | grep -v 'utest')
 clang-format -i $TO_FORMAT
+
+TO_FORMAT=$(find . -name '*.py' | grep -v specs)
+black $TO_FORMAT
