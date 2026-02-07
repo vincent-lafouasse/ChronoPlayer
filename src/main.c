8bae05fa0fb649f3a2e0f7fe3b4605b44027dc37
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-07 04:32:45 +0100

    investigate DSP registers

--- src/main.c
+++ src/main.c
@@ -12,14 +12,23 @@ struct BRR_Sample {
     uint16_t loop;   // where to go to block end
 };
 
-struct BRR_Sample identify_sample(const struct DSP_State dsp[static 1],
-                                  uint8_t voice)
+struct BRR_Sample identify_sample(const struct DSP_State dsp[static 1])
 {
-    assert(voice < 8);
-
     const uint8_t sample_table_msb = dsp->registers[0x5d];
     const uint16_t sample_table = u16_parse(0u, sample_table_msb);
     printf("samples start at 0x%04x\n", sample_table);
+
+    // voice 0 only for now
+    // msn is 0
+    const int8_t v0_voll = (int8_t)dsp->registers[0x00];
+    const int8_t v0_volr = (int8_t)dsp->registers[0x01];
+
+    const uint8_t v0_pitch_hl = dsp->registers[0x02];
+    const uint8_t v0_pitch_hh = dsp->registers[0x03] & 0x3f;
+    const uint16_t v0_pitch = u16_parse(v0_pitch_hl, v0_pitch_hh);
+
+    const uint8_t v0_srcn = dsp->registers[0x04];
+    const uint16_t v0_sample_location = sample_table + v0_srcn;
 
     struct BRR_Sample sample = {0};
 
@@ -33,5 +42,5 @@ int main(void)
     struct SPC_State spc_state;
     load_spc_or_exit(spc_path, &spc_state);
 
-    (void)identify_sample(&spc_state.dsp, 0);
+    (void)identify_sample(&spc_state.dsp);
 }
