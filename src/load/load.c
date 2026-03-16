bf179679677016dfe4ed3572144ae9645ca2efa8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-16 01:30:58 +0100

    make try_read static as file IO is only performed in load/

--- src/load/load.c
+++ src/load/load.c
@@ -17,7 +17,7 @@
 #include "utils.h"
 
 // failures to read (exact amount of) bytes are always fatal
-bool try_read(int fd, void* buffer, size_t sz)
+static bool try_read(int fd, void* buffer, size_t sz)
 {
     return read(fd, buffer, sz) == (ssize_t)sz;
 }
