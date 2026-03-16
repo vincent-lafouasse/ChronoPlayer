4f3792e70f66c750adde21cad802d49354bb3a7f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-16 02:28:24 +0100

    sketch voice_init

--- src/dsp/dsp_init.c
+++ src/dsp/dsp_init.c
@@ -2,6 +2,10 @@
 
 #include "dsp_internals.h"
 
+//static void voice_init(struct DSP_State dsp[static restrict 1],
+//                       const uint8_t registers[static restrict 128],
+//                       uint8_t voice_i);
+
 void dsp_init(struct DSP_State dsp[static restrict 1],
               const uint8_t registers[static restrict 128],
               const uint8_t aram[static restrict 0x10000])
