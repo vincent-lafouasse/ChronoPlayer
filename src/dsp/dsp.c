8e95c43b36940123742d779a4391c263af5de667
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-16 02:06:13 +0100

    introduce dsp_internals header

--- src/dsp/dsp.c
+++ src/dsp/dsp.c
@@ -1,5 +1,7 @@
 #include "dsp.h"
 
+#include "dsp_internals.h"
+
 void dsp_init(struct DSP_State dsp[static restrict 1],
               const uint8_t registers[static restrict 128],
               const uint8_t aram[static restrict 0x10000])
