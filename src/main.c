e1b8fe9eb3ecf1abc833f6f903fe42b25aa16e21
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-16 03:20:14 +0100

    remove dead inclusions

--- src/main.c
+++ src/main.c
@@ -1,16 +1,9 @@
 #include <stdbool.h>
-
-#include <assert.h>
 #include <stdio.h>
-
 #include <string.h>
 
 #include "dsp/dsp_internals.h"
-#include "dsp/brr.h"
-
 #include "load/load.h"
-
-#include "utils.h"
 
 int main(void)
 {
