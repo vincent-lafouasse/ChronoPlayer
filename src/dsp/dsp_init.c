acbdf3749ea31fd7962f34e221adab0028f90f26
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-16 02:41:59 +0100

    introduce dsp_latch_registers

--- src/dsp/dsp_init.c
+++ src/dsp/dsp_init.c
@@ -18,22 +18,26 @@ static void voice_init(struct DSP_State dsp[static restrict 1], uint8_t voice_i)
     voice_latch_registers(regs, voice);
 }
 
-void dsp_init(struct DSP_State dsp[static restrict 1],
-              const uint8_t registers[static restrict 128],
-              const uint8_t aram[static restrict 0x10000])
+static void dsp_latch_registers(struct DSP_State dsp[static restrict 1])
 {
-    memcpy(dsp->registers, registers, 128);
-
-    dsp->eon = registers[DSP_EON];
-    dsp->non = registers[DSP_NON];
-    dsp->dir = registers[DSP_DIR];
-
-    dsp->addr_latch = aram[0xf2];
+    dsp->eon = dsp->registers[DSP_EON];
+    dsp->non = dsp->registers[DSP_NON];
+    dsp->dir = dsp->registers[DSP_DIR];
 
     for (uint8_t i = 0; i < 8; i++) {
         const uint8_t addr = DSP_FFC(i);
         dsp->ffc[i] = dsp->registers[addr];
     }
+}
+
+void dsp_init(struct DSP_State dsp[static restrict 1],
+              const uint8_t registers[static restrict 128],
+              const uint8_t aram[static restrict 0x10000])
+{
+    memcpy(dsp->registers, registers, 128);
+    dsp->addr_latch = aram[0xf2];
+
+    dsp_latch_registers(dsp);
 
     for (uint8_t i = 0; i < 8; i++) {
         voice_init(dsp, i);
