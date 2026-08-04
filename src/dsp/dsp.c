982c7e3f5986582fbb63789525df171201792649
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-08-04 02:31:14 +0200

    simplify

--- src/dsp/dsp.c
+++ src/dsp/dsp.c
@@ -22,103 +22,73 @@ void dsp_tick(struct DSP_State dsp[static 1])
 {
     switch (dsp->cycle) {
         case 0:
-            dsp->cycle++;
             break;
         case 1:
-            dsp->cycle++;
             break;
         case 2:
-            dsp->cycle++;
             break;
         case 3:
-            dsp->cycle++;
             break;
         case 4:
-            dsp->cycle++;
             break;
         case 5:
-            dsp->cycle++;
             break;
         case 6:
-            dsp->cycle++;
             break;
         case 7:
-            dsp->cycle++;
             break;
         case 8:
-            dsp->cycle++;
             break;
         case 9:
-            dsp->cycle++;
             break;
         case 10:
-            dsp->cycle++;
             break;
         case 11:
-            dsp->cycle++;
             break;
         case 12:
-            dsp->cycle++;
             break;
         case 13:
-            dsp->cycle++;
             break;
         case 14:
-            dsp->cycle++;
             break;
         case 15:
-            dsp->cycle++;
             break;
         case 16:
-            dsp->cycle++;
             break;
         case 17:
-            dsp->cycle++;
             break;
         case 18:
-            dsp->cycle++;
             break;
         case 19:
-            dsp->cycle++;
             break;
         case 20:
-            dsp->cycle++;
             break;
         case 21:
-            dsp->cycle++;
             break;
         case 22:
-            dsp->cycle++;
             break;
         case 23:
-            dsp->cycle++;
             break;
         case 24:
-            dsp->cycle++;
             break;
         case 25:
-            dsp->cycle++;
             break;
         case 26:
-            dsp->cycle++;
             break;
         case 27:
-            dsp->cycle++;
             break;
         case 28:
-            dsp->cycle++;
             break;
         case 29:
-            dsp->cycle++;
             break;
         case 30:
-            dsp->cycle++;
             break;
         case 31:
-            dsp->cycle = 0;
             break;
         default:
             fprintf(stderr, "invalid dsp sub cycle: %i\n", dsp->cycle);
             UNREACHABLE();
     }
+    dsp->cycle++;
+    dsp->cycle %= 32;
 }
