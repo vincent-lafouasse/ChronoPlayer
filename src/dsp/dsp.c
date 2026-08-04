e9bb27f4341f5b8f9ec7221931afed2571ec7a1d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-08-04 02:12:18 +0200

    define dummy voice steps

--- src/dsp/dsp.c
+++ src/dsp/dsp.c
@@ -2,4 +2,19 @@
 
 #include "dsp_internals.h"
 
+#define DEFINE_DUMMY_STEP(index)                               \
+    void XXX_voice_step##index(struct DSP_State dsp[static 1], \
+                               uint8_t voice_i)                \
+    {                                                          \
+        (void)dsp;                                             \
+        (void)voice_i;                                         \
+    }
+
+DEFINE_DUMMY_STEP(4)
+DEFINE_DUMMY_STEP(5)
+DEFINE_DUMMY_STEP(6)
+DEFINE_DUMMY_STEP(7)
+DEFINE_DUMMY_STEP(8)
+DEFINE_DUMMY_STEP(9)
+
 void dsp_tick(struct DSP_State dsp[static 1]);
