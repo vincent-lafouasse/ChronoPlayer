41d572eb36dadb6bcdaff798a2835c07b2c67ba1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-08-05 03:16:02 +0200

    comment

--- src/dsp/dsp_internals.h
+++ src/dsp/dsp_internals.h
@@ -18,23 +18,28 @@
 #define DSP_V_ENVX(voice) ((voice) << 4 | 0x8)
 #define DSP_V_OUTX(voice) ((voice) << 4 | 0x9)
 
+// Master
 #define DSP_MVOLL (0x0c)
 #define DSP_MVOLR (0x1c)
-#define DSP_EVOLL (0x2c)
-#define DSP_EVOLR (0x3c)
-#define DSP_KON (0x4c)
-#define DSP_KOFF (0x5c)
 #define DSP_FLG (0x6c)
-#define DSP_ENDX (0x7c)
-
-#define DSP_EFB (0x0d)
 #define DSP_PMON (0x2d)
 #define DSP_NON (0x3d)
-#define DSP_EON (0x4d)
 #define DSP_DIR (0x5d)
+
+// Control
+#define DSP_KON (0x4c)
+#define DSP_KOFF (0x5c)
+#define DSP_ENDX (0x7c)
+
+// Echo
+#define DSP_EVOLL (0x2c)
+#define DSP_EVOLR (0x3c)
+#define DSP_EON (0x4d)
+#define DSP_EFB (0x0d)
 #define DSP_ESA (0x6d)
 #define DSP_EDL (0x7d)
 
+// FIR table: C0-C7
 #define DSP_FFC(i) ((i) << 4 | 0xf)
 
 void voice_step1(struct DSP_State dsp[static 1], uint8_t voice_i);
